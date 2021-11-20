489de1ee8b0fbddbbdb8f094a76e411dad2ebb23
jae-hun-kim <jae.hun.kim@navercorp.com>
2021-11-21 08:15:49 +0900

    Add profile

--- src/views/examples/ProfilePage.js
+++ src/views/examples/ProfilePage.js
@@ -1,16 +1,11 @@
 import React from "react";
+import styled from 'styled-components';
 
 // reactstrap components
 import {
-  Button,
-  Label,
-  FormGroup,
-  Input,
-  NavItem,
-  NavLink,
-  Nav,
-  TabContent,
-  TabPane,
+  Card,
+  CardBody,
+  CardTitle,
   Container,
   Row,
   Col,
@@ -21,15 +16,123 @@ import ColorNavbar from "components/Navbars/ColorNavbar.js";
 import ProfilePageHeader from "components/Headers/ProfilePageHeader.js";
 import FooterWhite from "components/Footers/FooterWhite.js";
 
+const Exhibitions = [
+  {
+    title: "Education",
+    info: [
+      "2015 M.F.A. Metalwork & Jewelry, Kookmin University, Seoul, Korea",
+      "2013 Exchange Student Program, Pforzheim University, Pforzheim, Germany",
+      "2012 B.F.A. Metalwork & Jewelry, Kookmin University, Seoul, Korea",    
+    ]
+  },
+  {
+    title:  "Solo Exhibition",
+    info: [
+      "2018 Captured moments, Gallery Ahwon, Seoul, Korea"
+    ]
+  },
+  {
+    title: "Group Exhibitions",
+    info: [
+      [
+        "2020",
+        "Lighten Up! – Jewelric rond, Crafts on the hill, Seoul",
+        "BKV-Prize 2020 for Young Applied Arts, Munich, Germany",
+      ],
+      [
+        "2019 ",
+        "Our necklace guide – Jewelric rondo, Craft trend fair, Seoul",
+        "Nest in a garden, Lee Eugean Gallery, Seoul",
+        "Alone or Together, Sanwoollim Art & Craft, Seoul",
+        "Analog_ Digital, Gallery Baum, Paju",
+        "720 Degrees, SNAG Chicago, Adorned spaces, Chicago, USA",
+        "Artmining Paris-Revelations, Grand palais, Paris, France",
+        "Artmining Milano, Palazzo Litta, Milano, Italy",
+        "COLLECT, Korea Craft & Design Foundation , Saatchi gallery, London, UK",
+      ],
+      [
+        "2018 ",
+        "SOFA Art Fair, Korea Craft & Design Foundation , Navy Pier, Chicago, USA",
+        "A little matter, Gallery OOOJH, Seoul, Korea",
+        "Artmining Seoul, DDP, Seoul, Korea",
+        "Jewelry breeze, 515 Gallery, Gwangju, Korea",
+        "Ring Ring Ring, Gallery An, Seoul, Korea",
+        "EVERYTHING UNDER THE SUN, Gallery toast, Seoul, Korea",
+        "-ing Creative Festival, Creative Market, Dubai, UAE",
+      ],
+      [
+        "2017 ",
+        "Hee-ang Kim Jewelry, Craft Trend Fair, COEX, Seoul, Korea",
+        "Participated in V&A-KCDF International Maker’s Workshop, V&A Museum, London, UK",
+        "Our Earring Guide, KCDF Gallery, Seoul, Korea",
+        "Ornamentation, Yoolizzy Craft Museum, Seoul, Korea",
+      ],
+      [
+        "2016  ",
+        "White Christmas, Galeria Tereza Seabra, Lisbon, Portugal",
+        "Jewelric Rondo, Craft Trend Fair, COEX, Seoul, Korea",
+        "20/20, Kath Libbert Jewellery Gallery, Bradford, UK",
+        "ART OF AURA, Tumtree Project, Daegu, Korea",
+        "Maison & Object, Korea Craft & Design Foundation, Paris Nord Villepinte, France",
+      ],
+      [
+        "2015 ",
+        "Hee-ang Kim Jewelry, Craft Trend Fair, COEX, Seoul, Korea",
+        "JOYA Barcelona Art Jewellery Fair, Arts Santa Monica, Barcelona, Spain",
+        "BKV-PRIZE 2015 For Young Applied Arts, Munich, Germany",
+        "Marzee International Graduate Show, Galerie Marzee, Nijmegen, Netherlands",
+        "Taiwan International Metal Crafts Competition, Gold Museum, New Taipei City, Taiwan",
+        "Art and Ecology, Seoho museum of art, Namyangju, Korea",
+      ],
+      [
+        "2014 ",
+        "Graduate Thesis Exhibition, Chohyung Gallery, Kookmin University, Seoul, Korea",
+      ],
+      [
+        "2012 ",
+        "ONE-OFF, Craft Trend Fair, COEX, Seoul, Korea",
+        "Wishlist, Sungkok Art Museum, Seoul, Korea",
+        "Alchemists- Mythology of Material, KCDF Gallery, Seoul, Korea",
+        "80-90 sensitive image, Sungkok Art Museum, Seoul, Korea",
+      ],
+      [
+        "2011 ",
+        "ONE-OFF, Seoul Design Festival, COEX, Seoul, Korea",
+        "Beautiful Object – Graduation Exhibition, Dongduk Art Gallery, Seoul, Korea",
+      ]
+    ].map((obj)=>{
+      return obj.map(str => <p>{str}</p>);
+    })
+  },
+  {
+    title: "Awards & Collections",
+    info: [
+      "2020 BKV-PRIZE 2020 For Young Applied Arts, Finalist, Munich, Germany",
+      "2016 Alliages – Legacy Award, Permanent Collection, Espace Solidor, Cagnes-sur-Mer, France",
+      "2015 BKV-PRIZE 2015 For Young Applied Arts, Finalist, Munich, Germany",
+      "2015 Taiwan International Metal Craft Competition, Quality Award, New Taipei City, Taiwan",
+      "2012 The 12th Korea Eyewear Design Competition, Selected, Daegu, korea",
+    ]
+  },
+  {
+    title: "Publication",
+    info: [
+      "2020 Polymer Week #2",
+      "2019 <Homeliving & Style> October issue",
+      "2016 AUTOR Contemporary Jewelry Magazine #3",
+    ]
+  },
+  {
+    title: "Lecture & Workshop",
+    info: [
+      "2018-1, 2019-1 Lecturer at Myongji College, Seoul, Korea",
+      "2017 Participated in V&A-KCDF International Maker’s Workshop, V&A Museum, London, UK",
+      "2017 Lecture for ‘Culture day’ Polymer clay class at the Yoolizzy Craft Museum, Seoul, Korea",
+    ]
+  }
+];
+
 function ProfilePage() {
-  const [activeTab, setActiveTab] = React.useState("1");
-
-  const toggle = (tab) => {
-    if (activeTab !== tab) {
-      setActiveTab(tab);
-    }
-  };
-
   document.documentElement.classList.remove("nav-open");
   React.useEffect(() => {
     document.body.classList.add("profile-page");
@@ -54,139 +157,42 @@ function ProfilePage() {
                     <img
                       alt="..."
                       src={
-                        require("assets/img/faces/joe-gardner-2.jpg").default
+                        require("assets/img/03_profile/profile.jpg").default
                       }
                     />
                   </div>
                   <div className="name">
                     <h4 className="title text-center">
-                      Chet Faker <br />
-                      <small>Music Producer</small>
+                      김희앙, Hee-ang Kim <br />
+                      <small>Jeweller</small>
                     </h4>
                   </div>
                 </div>
               </div>
             </Row>
             <Row>
-              <Col className="ml-auto mr-auto text-center" md="6">
-                <p>
-                  An artist of considerable range, Chet Faker — the name taken
-                  by Melbourne-raised, Brooklyn-based Nick Murphy — writes,
-                  performs and records all of his own music, giving it a warm,
-                  intimate feel with a solid groove structure.
-                </p>
-                <br />
-                <Button className="btn-round" color="default" outline>
-                  <i className="fa fa-cog mr-1" />
-                  Settings
-                </Button>
+              <Col className="ml-auto mr-auto text-center" md="8">
+                <Card className="card-blog card-exhibition">
+                  {Exhibitions.map((obj) => {
+                    return (
+                      <CardBody>
+                        <CardTitle tag="h3">
+                          {obj.title}
+                        </CardTitle>
+                        {obj.info.map((line) => {
+                          return (
+                            <div className="exhibition-description">
+                              <p>{line}</p>
+                            </div>
+                          );
+                        })}
+                      </CardBody>
+                    );
+                  })}
+                </Card>
               </Col>
             </Row>
             <br />
-            <div className="nav-tabs-navigation">
-              <div className="nav-tabs-wrapper">
-                <Nav role="tablist" tabs>
-                  <NavItem>
-                    <NavLink
-                      className={activeTab === "1" ? "active" : ""}
-                      onClick={() => {
-                        toggle("1");
-                      }}
-                    >
-                      Follows
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      className={activeTab === "2" ? "active" : ""}
-                      onClick={() => {
-                        toggle("2");
-                      }}
-                    >
-                      Following
-                    </NavLink>
-                  </NavItem>
-                </Nav>
-              </div>
-            </div>
-            {/* Tab panes */}
-            <TabContent className="following" activeTab={activeTab}>
-              <TabPane tabId="1" id="follows">
-                <Row>
-                  <Col className="ml-auto mr-auto" md="6">
-                    <ul className="list-unstyled follows">
-                      <li>
-                        <Row>
-                          <Col className="ml-auto mr-auto" lg="2" md="4" xs="4">
-                            <img
-                              alt="..."
-                              className="img-circle img-no-padding img-responsive"
-                              src={
-                                require("assets/img/faces/clem-onojeghuo-3.jpg")
-                                  .default
-                              }
-                            />
-                          </Col>
-                          <Col className="ml-auto mr-auto" lg="7" md="4" xs="4">
-                            <h6>
-                              Lincoln <br />
-                              <small>Car Producer</small>
-                            </h6>
-                          </Col>
-                          <Col className="ml-auto mr-auto" lg="3" md="4" xs="4">
-                            <FormGroup check>
-                              <Label check>
-                                <Input
-                                  defaultChecked
-                                  defaultValue=""
-                                  type="checkbox"
-                                />
-                                <span className="form-check-sign" />
-                              </Label>
-                            </FormGroup>
-                          </Col>
-                        </Row>
-                      </li>
-                      <hr />
-                      <li>
-                        <Row>
-                          <Col className="mx-auto" lg="2" md="4" xs="4">
-                            <img
-                              alt="..."
-                              className="img-circle img-no-padding img-responsive"
-                              src={
-                                require("assets/img/faces/erik-lucatero-2.jpg")
-                                  .default
-                              }
-                            />
-                          </Col>
-                          <Col lg="7" md="4" xs="4">
-                            <h6>
-                              Banks <br />
-                              <small>Singer</small>
-                            </h6>
-                          </Col>
-                          <Col lg="3" md="4" xs="4">
-                            <FormGroup check>
-                              <Label check>
-                                <Input defaultValue="" type="checkbox" />
-                                <span className="form-check-sign" />
-                              </Label>
-                            </FormGroup>
-                          </Col>
-                        </Row>
-                      </li>
-                    </ul>
-                  </Col>
-                </Row>
-              </TabPane>
-              <TabPane className="text-center" tabId="2" id="following">
-                <h3 className="text-muted">Not following anyone yet :(</h3>
-                <Button className="btn-round" color="warning">
-                  Find artists
-                </Button>
-              </TabPane>
-            </TabContent>
           </Container>
         </div>
       </div>
