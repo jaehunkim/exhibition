7afdc4f00cffbfceb4d0ceb07ce4d4d907fb5698
jaehunkim <jaehunkim@outlook.com>
2021-11-20 22:28:33 +0900

    Initial commit (#1)
    Co-authored-by: jae-hun-kim <jae.hun.kim@navercorp.com>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/scss/bootstrap.scss";
+import "assets/scss/paper-kit.scss";
+import "assets/demo/demo.css";
+import "assets/demo/react-demo.css";
+// pages
+import Index from "views/Index.js";
+import Sections from "views/Sections.js";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/index" render={(props) => <Index {...props} />} />
+      <Route path="/sections" render={(props) => <Sections {...props} />} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
