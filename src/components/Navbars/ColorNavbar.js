fbbdc2ae3c92e80b55dec241ed9dd5f2cbd42286
jaehunkim <jaehunkim@outlook.com>
2021-11-21 01:34:19 +0900

    Index page (#2)
    * Add fabicon
    
    * Complete index
    
    Co-authored-by: jae-hun-kim <jae.hun.kim@navercorp.com>

--- src/components/Navbars/ColorNavbar.js
+++ src/components/Navbars/ColorNavbar.js
@@ -6,7 +6,6 @@ import classnames from "classnames";
 import Headroom from "headroom.js";
 // reactstrap components
 import {
-  Button,
   Collapse,
   DropdownToggle,
   DropdownMenu,
@@ -15,6 +14,7 @@ import {
   NavbarBrand,
   Navbar,
   NavItem,
+  NavLink,
   Nav,
   Container,
   UncontrolledTooltip,
@@ -67,10 +67,10 @@ function ColorNavbar() {
         <Container>
           <div className="navbar-translate">
             <NavbarBrand id="navbar-brand" to="/index" tag={Link}>
-              Paper Kit PRO React
+              Leap Of Everyday
             </NavbarBrand>
             <UncontrolledTooltip placement="bottom" target="navbar-brand">
-              Paper Kit PRO React
+            Leap Of Everyday
             </UncontrolledTooltip>
             <button
               className="navbar-toggler"
@@ -89,141 +89,36 @@ function ColorNavbar() {
           </div>
           <Collapse navbar isOpen={collapseOpen}>
             <Nav className="ml-auto" navbar>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle className="mr-2" color="default" caret nav>
-                  Components
-                </DropdownToggle>
-                <DropdownMenu className="dropdown-danger" right>
-                  <DropdownItem to="/index" tag={Link}>
-                    All Components
-                  </DropdownItem>
-                  <DropdownItem to="/presentation" tag={Link}>
-                    Presentation
-                  </DropdownItem>
-                  <DropdownItem
-                    href="https://demos.creative-tim.com/paper-kit-pro-react/#/documentation/introduction?ref=pkpr-color-navbar"
-                    target="_blank"
-                  >
-                    Documentation
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle className="mr-2" color="default" caret nav>
-                  Sections
-                </DropdownToggle>
-                <DropdownMenu className="dropdown-danger" right>
-                  <DropdownItem to="/sections#headers" tag={Link}>
-                    <i className="nc-icon nc-tile-56" />
-                    Headers
-                  </DropdownItem>
-                  <DropdownItem to="/sections#features" tag={Link}>
-                    <i className="nc-icon nc-settings" />
-                    Features
-                  </DropdownItem>
-                  <DropdownItem to="/sections#blogs" tag={Link}>
-                    <i className="nc-icon nc-bullet-list-67" />
-                    Blogs
-                  </DropdownItem>
-                  <DropdownItem to="/sections#teams" tag={Link}>
-                    <i className="nc-icon nc-single-02" />
-                    Teams
-                  </DropdownItem>
-                  <DropdownItem to="/sections#projects" tag={Link}>
-                    <i className="nc-icon nc-calendar-60" />
-                    Projects
-                  </DropdownItem>
-                  <DropdownItem to="/sections#pricing" tag={Link}>
-                    <i className="nc-icon nc-money-coins" />
-                    Pricing
-                  </DropdownItem>
-                  <DropdownItem to="/sections#testimonials" tag={Link}>
-                    <i className="nc-icon nc-badge" />
-                    Testimonials
-                  </DropdownItem>
-                  <DropdownItem to="/sections#contact-us" tag={Link}>
-                    <i className="nc-icon nc-mobile" />
-                    Contact Us
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
+              <NavItem>
+                <NavLink href="#pablo" onClick={e => e.preventDefault()}>
+                  Home
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#pablo" onClick={e => e.preventDefault()}>
+                  Exhibition
+                </NavLink>
+              </NavItem>
               <UncontrolledDropdown nav inNavbar>
                 <DropdownToggle color="default" caret nav>
-                  Examples
+                  More
                 </DropdownToggle>
                 <DropdownMenu className="dropdown-danger" right>
-                  <DropdownItem to="/about-us" tag={Link}>
-                    <i className="nc-icon nc-bank" />
-                    About-us
-                  </DropdownItem>
-                  <DropdownItem to="/add-product" tag={Link}>
-                    <i className="nc-icon nc-basket" />
-                    Add Product
-                  </DropdownItem>
-                  <DropdownItem to="/blog-post" tag={Link}>
-                    <i className="nc-icon nc-badge" />
-                    Blog Post
-                  </DropdownItem>
-                  <DropdownItem to="/blog-posts" tag={Link}>
-                    <i className="nc-icon nc-bullet-list-67" />
-                    Blog Posts
-                  </DropdownItem>
-                  <DropdownItem to="/contact-us" tag={Link}>
-                    <i className="nc-icon nc-mobile" />
-                    Contact Us
-                  </DropdownItem>
-                  <DropdownItem to="/discover" tag={Link}>
-                    <i className="nc-icon nc-world-2" />
-                    Discover
-                  </DropdownItem>
-                  <DropdownItem to="/e-commerce" tag={Link}>
-                    <i className="nc-icon nc-send" />
-                    Ecommerce
-                  </DropdownItem>
-                  <DropdownItem to="/landing-page" tag={Link}>
-                    <i className="nc-icon nc-spaceship" />
-                    Landing Page
-                  </DropdownItem>
-                  <DropdownItem to="/login-page" tag={Link}>
-                    <i className="nc-icon nc-lock-circle-open" />
-                    Login Page
-                  </DropdownItem>
-                  <DropdownItem to="/product-page" tag={Link}>
-                    <i className="nc-icon nc-album-2" />
-                    Product Page
-                  </DropdownItem>
                   <DropdownItem to="/profile-page" tag={Link}>
                     <i className="nc-icon nc-single-02" />
                     Profile Page
                   </DropdownItem>
-                  <DropdownItem to="/register-page" tag={Link}>
-                    <i className="nc-icon nc-bookmark-2" />
-                    Register Page
+
+                  <DropdownItem to="/official-home" tag={Link}>
+                    <i className="nc-icon nc-bank" />
+                    heeang.com
                   </DropdownItem>
-                  <DropdownItem to="/search-with-sidebar" tag={Link}>
-                    <i className="nc-icon nc-zoom-split" />
-                    Search Page
-                  </DropdownItem>
-                  <DropdownItem to="/settings" tag={Link}>
-                    <i className="nc-icon nc-settings-gear-65" />
-                    Settings Page
-                  </DropdownItem>
-                  <DropdownItem to="/twitter-redesign" tag={Link}>
-                    <i className="nc-icon nc-tie-bow" />
-                    Twitter
+                  <DropdownItem to="/official-home" tag={Link}>
+                    <i className="fa fa-instagram" />
+                    instagram
                   </DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
-              <NavItem>
-                <Button
-                  className="btn-round"
-                  color="danger"
-                  href="https://www.creative-tim.com/product/paper-kit-pro-react?ref=pkpr-color-navbar"
-                  target="_blank"
-                >
-                  <i className="nc-icon nc-cart-simple" /> Buy Now
-                </Button>
-              </NavItem>
             </Nav>
           </Collapse>
         </Container>
--- src/index.js
+++ src/index.js
@@ -2,28 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import "bootstrap/scss/bootstrap.scss";
 import "assets/scss/paper-kit.scss";
 import "assets/demo/demo.css";
 import "assets/demo/react-demo.css";
 // pages
 import Index from "views/Index.js";
-import Sections from "views/Sections.js";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/index" render={(props) => <Index {...props} />} />
-      <Route path="/sections" render={(props) => <Sections {...props} />} />
+      <Redirect to="/index" />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
